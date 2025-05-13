af8229383f0d31460f4d3b58b6fc558594af2b35
André Luiz <126507217+andreluizhla@users.noreply.github.com>
2025-05-12 23:34:58 -0300

    added creation -ish

--- test-project/todo/views.py
+++ test-project/todo/views.py
@@ -10,10 +10,15 @@
 #     )
 
 # Código Atual:
-from django.views.generic import ListView
+from django.views.generic import ListView, CreateView
+from django.urls import reverse_lazy
 
 from .models import Todo
 
 class TodoListView(ListView):
     model = Todo
 
+class TodoCreateView(CreateView):
+    model = Todo
+    fields = ['title', 'deadline']
+    success_url = reverse_lazy("todo_list")
